c37ebf5de1ffa112514a3a6c43f7081eab296969
Aayush <kumar.aayush2310@gmail.com>
2023-07-08 09:42:30 +0530

    improved ui, replaced inkwell with text, added scaffold upon seat press

--- lib/home.dart
+++ lib/home.dart
@@ -8,7 +8,31 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final _messangerKey = GlobalKey<ScaffoldMessengerState>();
   int _a = 1, _b = 2, _c = 3, _d = 7, _e = 4, _f = 5, _g = 6, _h = 8;
+
+  String showInfo(int seatNum) {
+    String seatCategory = "", RowNum = "";
+    if (seatNum % 8 == 0) {
+      seatCategory = "Side Lower";
+      RowNum = ((seatNum / 8).truncate() + 1).toString();
+    } else if (seatNum % 8 == 1 || seatNum % 8 == 4) {
+      seatCategory = "Lower";
+      RowNum = ((seatNum / 8).truncate() + 1).toString();
+    } else if (seatNum % 8 == 2 || seatNum % 8 == 5) {
+      seatCategory = "Middle";
+      RowNum = ((seatNum / 8) + 1).toString();
+    } else if (seatNum % 8 == 3 || seatNum % 8 == 6) {
+      seatCategory = "Upper";
+      RowNum = ((seatNum / 8) + 1).toString();
+    } else if (seatNum % 8 == 7) {
+      seatCategory = "Side Upper";
+      RowNum = ((seatNum / 8) + 1).toString();
+    }
+
+    return "The seat is $seatCategory in row number: $RowNum.";
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -43,11 +67,18 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
               ),
+              const Padding(
+                padding: EdgeInsets.only(bottom: 20),
+              ),
               Expanded(
                 child: ListView.builder(
                   itemCount: 9,
                   itemBuilder: ((context, index) {
-                    return Card(
+                    return Container(
+                      margin: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          color: Colors.lightBlueAccent.shade100,
+                          borderRadius: BorderRadius.circular(20)),
                       child: Column(
                         children: [
                           Row(
@@ -55,23 +86,59 @@ class _HomeScreenState extends State<HomeScreen> {
                             children: [
                               Row(
                                 children: [
-                                  Text(
-                                    (_a + (index * 8)).toString(),
-                                    style: MyTextStyle.textStyle,
+                                  Container(
+                                    width: 40,
+                                    margin: const EdgeInsets.all(10),
+                                    color: Colors.pinkAccent.shade100,
+                                    padding: const EdgeInsets.all(5),
+                                    child: InkWell(
+                                      onTap: () => ScaffoldMessenger.of(context)
+                                          .showSnackBar(SnackBar(
+                                              content: Text(showInfo(
+                                                  (_a + (8 * index)))))),
+                                      child: Text(
+                                        (_a + (8 * index)).toString(),
+                                        style: MyTextStyle.textStyle,
+                                      ),
+                                    ),
                                   ),
-                                  Text(
-                                    (_b + (index * 8)).toString(),
-                                    style: MyTextStyle.textStyle,
+                                  Container(
+                                    width: 40,
+                                    margin: const EdgeInsets.all(10),
+                                    color: Colors.lightGreenAccent.shade100,
+                                    padding: const EdgeInsets.all(5),
+                                    child: InkWell(
+                                      child: Text(
+                                        (_b + (8 * index)).toString(),
+                                        style: MyTextStyle.textStyle,
+                                      ),
+                                    ),
                                   ),
-                                  Text(
-                                    (_c + (index * 8)).toString(),
-                                    style: MyTextStyle.textStyle,
+                                  Container(
+                                    width: 40,
+                                    margin: const EdgeInsets.all(10),
+                                    color: Colors.orangeAccent.shade100,
+                                    padding: const EdgeInsets.all(5),
+                                    child: InkWell(
+                                      child: Text(
+                                        (_c + (8 * index)).toString(),
+                                        style: MyTextStyle.textStyle,
+                                      ),
+                                    ),
                                   ),
                                 ],
                               ),
-                              Text(
-                                (_d + (index * 8)).toString(),
-                                style: MyTextStyle.textStyle,
+                              Container(
+                                width: 40,
+                                margin: const EdgeInsets.all(10),
+                                color: Colors.redAccent.shade100,
+                                padding: EdgeInsets.all(5),
+                                child: InkWell(
+                                  child: Text(
+                                    (_d + (8 * index)).toString(),
+                                    style: MyTextStyle.textStyle,
+                                  ),
+                                ),
                               ),
                             ],
                           ),
@@ -80,26 +147,61 @@ class _HomeScreenState extends State<HomeScreen> {
                             children: [
                               Row(
                                 children: [
-                                  Text(
-                                    (_e + (index * 8)).toString(),
-                                    style: MyTextStyle.textStyle,
+                                  Container(
+                                    width: 40,
+                                    margin: const EdgeInsets.all(10),
+                                    color: Colors.pinkAccent.shade100,
+                                    padding: EdgeInsets.all(5),
+                                    child: InkWell(
+                                      child: Text(
+                                        (_e + (8 * index)).toString(),
+                                        style: MyTextStyle.textStyle,
+                                      ),
+                                      onTap: () {
+                                        debugPrint("abc");
+                                      },
+                                    ),
                                   ),
-                                  Text(
-                                    (_f + (index * 8)).toString(),
-                                    style: MyTextStyle.textStyle,
+                                  Container(
+                                    width: 40,
+                                    margin: const EdgeInsets.all(10),
+                                    color: Colors.lightGreenAccent.shade100,
+                                    padding: EdgeInsets.all(5),
+                                    child: InkWell(
+                                      child: Text(
+                                        (_f + (8 * index)).toString(),
+                                        style: MyTextStyle.textStyle,
+                                      ),
+                                    ),
                                   ),
-                                  Text(
-                                    (_g + (index * 8)).toString(),
-                                    style: MyTextStyle.textStyle,
+                                  Container(
+                                    width: 40,
+                                    margin: const EdgeInsets.all(10),
+                                    color: Colors.orangeAccent.shade100,
+                                    padding: EdgeInsets.all(5),
+                                    child: InkWell(
+                                      child: Text(
+                                        (_g + (8 * index)).toString(),
+                                        style: MyTextStyle.textStyle,
+                                      ),
+                                    ),
                                   ),
                                 ],
                               ),
-                              Text(
-                                (_h + (index * 8)).toString(),
-                                style: MyTextStyle.textStyle,
+                              Container(
+                                width: 40,
+                                margin: const EdgeInsets.all(10),
+                                color: Colors.redAccent.shade100,
+                                padding: EdgeInsets.all(5),
+                                child: InkWell(
+                                  child: Text(
+                                    (_h + (8 * index)).toString(),
+                                    style: MyTextStyle.textStyle,
+                                  ),
+                                ),
                               ),
                             ],
-                          )
+                          ),
                         ],
                       ),
                     );
@@ -120,3 +222,5 @@ class MyTextStyle {
     fontWeight: FontWeight.bold,
   );
 }
+
+// Widget layOut(String seatNum) {}
